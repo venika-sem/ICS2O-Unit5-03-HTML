8d7a92fdd49576e7a00949f06b5abfc515d14826
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-04-29 17:29:20 +0000

    changed files

--- js/script.js
+++ js/script.js
@@ -9,21 +9,27 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit5-02-HTML/sw.js", {
-    scope: "/ICS2O-Unit5-02-HTML/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-03-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-03-HTML/",
   })
 }
 
 /**
- * This function checks if a number is negative or positive.
+ * This function determines if a person is eligible for movie rates.
  */
-function check() {
-  const userInteger = parseInt(document.getElementById("number").value);
-  if (userInteger > -1) {
-    document.getElementById("answer").innerHTML =
-      "The integer input is positive.";
+function myButtonClicked() {
+  //input
+  const seventeenAbove = document.getElementById("seventeenAbove").checked
+  const thirteenToSixteen = document.getElementById("thirteenToSixteen").checked
+  const fiveToTwelve = document.getElementById("fiveToTwelve").checked
+  //process
+  if (seventeenAbove == true) {
+    document.getElementById("answers").innerHTML = "You are eligible to watch R rated movies alone."
+  } else if (thirteenToSixteen == true) {
+      document.getElementById("answers").innerHTML = "You are eligible to watch PG 13 rated movies alone."
+  } else if (fiveToTwelve == true) {
+      document.getElementById("answers").innerHTML = "You are eligible to watch G and PG rated movies alone."
   } else {
-    document.getElementById("answer").innerHTML =
-      "The integer input is a negative.";
+      document.getElementById("answers").innerHTML = "You are too young to watch any rated movies alone."
   }
 }
